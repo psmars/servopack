1aee841a40b8b7a4f03f369dffb81ed60a870aa8
Pierre SMARS <smars@yuntech.edu.tw>
2018-02-19 11:51:53 +0800

    added easy c program to produce train of pulses (binary)

---
+++ preprocess/codes2pulses.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+//***************************************
+struct wave
+{
+	uint16_t wait;
+	char code;
+};
+//***************************************
+int main(int argc, char** argv)
+{
+	struct wave awave;
+	char code;
+	code = 'a';
+	uint16_t wait;
+	FILE *in;
+	char buffer[255];
+	in=fopen("pulses.bin","w");
+	while (!feof(stdin))
+	{
+		if (fscanf(stdin,"%s %c", buffer,&code)==2)
+		{
+			char code2;
+			code2 = ((code<<1)&0x40)|(code<<2)&0x7c;
+	    awave.wait = atoi(buffer);
+	    awave.code = code2;
+	    printf("wait:%lu code:%c\n",atoi(buffer),code);
+	    fwrite(&awave,sizeof(awave)-1,1,in);
+		}
+	}
+	fclose(in);
+	return 0;
+}
