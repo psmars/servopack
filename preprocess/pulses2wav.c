b0324b0ed75718a11cf521b552ab1075d6f3cb67
Pierre SMARS <smars@yuntech.edu.tw>
2019-12-30 00:10:41 +0800

    added pulses2wav to reverse the action of quantise: wip

---
+++ preprocess/pulses2wav.c
@@ -0,0 +1,48 @@
+/*
+	 Copyright 2019 Pierre SMARS (smars@yuntech.edu.tw)
+	 This program is free software: you can redistribute it and/or modify
+	 it under the terms of the GNU General Public License as published by
+	 the Free Software Foundation, either version 2 of the License, or
+	 (at your option) any later version.
+
+	 This program is distributed in the hope that it will be useful,
+	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 GNU General Public License for more details.
+
+	 You should have received a copy of the GNU General Public License
+	 along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	 */
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int main()
+{
+	int x,y;
+	uint16_t wait;
+	char code;
+	while (!feof(stdin))    
+  {    
+		fread(&wait,sizeof(wait),1,stdin);
+		fread(&code,sizeof(code),1,stdin);
+		char code2 = (code&0x3c)>>0;
+		short d2 = code2&0x1;
+		short p2 = (code2>>1)&0x1;
+		short d1 = (code2>>2)&0x1;
+		short p1 = (code2>>3)&0x1;
+		if (d1==1)
+			x -= p1;
+		else
+			x += p1;
+		if (d2==1)
+			y -= p2;
+		else
+			y += p2;
+		for (int i=0; i<wait/10; i++)
+		{
+			printf("%d %d\n",x,y);
+		}
+	}
+	return 0;
+}
--- preprocess/quantise.cxx
+++ preprocess/quantise.cxx
@@ -135,7 +135,7 @@ void output(const unsigned int t, const short p1, const short p2)
 	if (binary)
 	{
 		uint16_t wait = t0;
-		char trig = 0x60 | ((p1!=0)<<4) | ((p1<1)<<3) | ((p2!=0)<<2) | ((p2<1)<<1); 
+		char trig = 0x60 | ((p1!=0)<<3) | ((p1<0)<<2) | ((p2!=0)<<1) | ((p2<0)<<0); 
 		fout.write((char *)&wait,sizeof(wait));
 		fout << trig;
 	}
