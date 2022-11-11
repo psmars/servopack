ff264a5904fcab4966097a7158e948fee38184f0
Pierre SMARS <smars@yuntech.edu.tw>
2022-11-11 13:50:52 +0800

    burst: catch errors + help

--- preprocess/burst.m
+++ preprocess/burst.m
@@ -7,12 +7,17 @@
 #sig: signal (m)
 #n: number of samples
 function [t, sig, n] = burst (freq, ag, time, dt)
-	w=2*pi*freq;
-	t=(0:dt:time)';
-	n=size(t)(1);
-	f2=1./(2*time);
-	w2=2*pi*f2;
-	a=9.81*ag;
-	d=a/(w*w);
-	sig=d*sin(w2*t).*sin(w*t);
+	try
+		w=2*pi*freq;
+		t=(0:dt:time)';
+		n=size(t)(1);
+		f2=1./(2*time);
+		w2=2*pi*f2;
+		a=9.81*ag;
+		d=a/(w*w);
+		sig=d*sin(w2*t).*sin(w*t);
+	catch
+		disp("create a burst time series");
+		disp("[time, signal, number_of_sample] = burst (freq (Hz), ag (g), time (s), dt (s) )");
+	end_try_catch
 endfunction
