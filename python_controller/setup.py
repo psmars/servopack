f9462abbe19e31c87e3f310633a5b372ba6e5f2b
Pierre SMARS <smars@yuntech.edu.tw>
2023-05-25 18:15:56 +0800

    completed setup.py

--- python_controller/setup.py
+++ python_controller/setup.py
@@ -2,15 +2,18 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="servo",
-    version="0.1",
-    description = "shaking table control",
-    author = "Pierre SMARS",
-    url = "https://github.com/psmars/servopack",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    scripts=['src/run','src/transfer'],
-    install_requires=["matplotlib==3.7.1"],
-    license = "GNU General Public License ver. 2",
-)
+        name="servo",
+        version="0.1",
+        description="Shaking table control library and scripts",
+        long_description="2 axis shaking table, servo motor control library and scripts",
+        author="Pierre SMARS",
+        author_email="smars@yuntech.edu.tw",
+        url="https://github.com/psmars/servopack",
+        license="GPLv2",
+        platforms=["any"],
+        packages=find_packages(where="src"),
+        package_dir={"": "src"},
+        scripts=['src/run','src/transfer'],
+        install_requires=["matplotlib==3.7.1"],
+        )
 
