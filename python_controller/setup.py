25317a24b0c6936565cbff6a1e6c656e8b7f7bfd
Pierre SMARS <smars@yuntech.edu.tw>
2025-01-22 21:22:23 +0800

    install properly servo package

--- python_controller/setup.py
+++ python_controller/setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
         name="servo",
-        version="0.1",
+        version="0.2",
         description="Shaking table control library and scripts",
         long_description="2 axis shaking table, servo motor control library and scripts",
         author="Pierre SMARS",
@@ -11,9 +11,9 @@ setup(
         url="https://github.com/psmars/servopack",
         license="GPLv2",
         platforms=["any"],
-        packages=find_packages(where="src"),
+        packages=find_packages(where="."),
         package_dir={"": "src"},
         scripts=['src/run','src/transfer'],
-        install_requires=["matplotlib==3.7.1"],
+        install_requires=["matplotlib==3.10.0"],
         )
 
